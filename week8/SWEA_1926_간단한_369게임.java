d301ea4c14e18d5dcc94fe91070bacbc526f70e8
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-07-28 00:01:16 +0900

    SWEA D2

---
+++ week8/SWEA_1926_간단한_369게임.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N;
+		N=sc.nextInt();
+        
+        for(int i=1; i<=N; i++) {
+            int ii = i;
+            StringBuilder result = new StringBuilder("");
+            
+            while(ii>0) {
+                if(ii % 10 == 3 || ii%10 == 6 || ii%10 == 9) result.append("-");
+                ii = ii/10;
+            }
+            
+            String str = result.toString();
+            if(str.contains("-")) {
+                result.append(" ");
+                System.out.print(result);
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+	}
+}
