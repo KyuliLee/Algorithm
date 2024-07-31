1821fc473759d88783c185397df88a483202d749
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-07-31 23:29:17 +0900

    정렬

---
+++ week9/PGS_K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<answer.length; i++) {
+            int from = commands[i][0]-1;
+            int to = commands[i][1];
+            int[] arr = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
