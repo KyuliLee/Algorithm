d6ce6608c5969005f5da10b8077f50ac07fac70e
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-07-31 23:54:15 +0900

    해시

---
+++ week9/PGS_폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        // nums의 값들을 Set에 넣어서 중복을 제거
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            set.add(nums[i]);
+        }
+        if((nums.length/2) <= set.size()) {
+            return nums.length/2;
+        }
+        return set.size();
+    }
+}
