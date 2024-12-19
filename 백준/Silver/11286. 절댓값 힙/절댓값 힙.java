8b0b44a2dcc58ae41bcda578c19f79b538067ea4
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-12-19 15:02:03 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -8,46 +8,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> plus = new PriorityQueue<>(); // 오름차순 정렬
-        PriorityQueue<Integer> minus = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> {
+            int abs1 = Math.abs(n1);
+            int abs2 = Math.abs(n2);
+            if(abs1 == abs2) {
+                return n1-n2;
+            }
+            return abs1 - abs2;
+        });
         StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<N; i++) {
             int n = Integer.parseInt(br.readLine());
-            if(n>0) {
-                plus.offer(n);
-                continue;
-            }
-            if(n<0) {
-                minus.offer(n);
-                continue;
-            }
-            // n==0이고 plus, minus 둘 다 비어있을 때
-            if(plus.isEmpty() && minus.isEmpty()) {
-                sb.append("0").append("\n");
-                continue;
-            }
-            // n==0이고 minus에만 값이 있을 때
-            if(plus.isEmpty() && !minus.isEmpty()) {
-                sb.append(minus.poll()).append("\n");
-                continue;
-            }
-            // n==0이고 plus에만 값이 있을 때
-            if(!plus.isEmpty() && minus.isEmpty()) {
-                sb.append(plus.poll()).append("\n");
-                continue;
-            }
-            // n==0이고 둘 다 값이 있을 때 : peek으로 절댓값 비교
-            int p = plus.peek();
-            int m = minus.peek()*-1;
-            if(p<m) {
-                sb.append(plus.poll()).append("\n");
-            } else if(p>m) {
-                sb.append(minus.poll()).append("\n");
+            if(n==0){
+                if(pq.isEmpty()) {
+                    sb.append("0").append("\n");
+                    continue;
+                }
+                sb.append(pq.poll()).append("\n");
             } else {
-                sb.append(minus.poll()).append("\n");
+                pq.offer(n);
             }
+
         }
+
         System.out.println(sb);
     }
 }
