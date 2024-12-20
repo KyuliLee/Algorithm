574cb5936b1f7ca294698432e7958a3dcd5c2f7b
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2024-12-20 10:35:00 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 15.54 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,137 +1,57 @@
-import java.util.*;
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int maxmm = convertToInt(video_len.substring(0, 2));
-        int maxss = convertToInt(video_len.substring(3, 5));
-        int mm = convertToInt(pos.substring(0, 2));
-        int ss = convertToInt(pos.substring(3, 5));
-        int startmm = convertToInt(op_start.substring(0, 2));
-        int startss = convertToInt(op_start.substring(3, 5));
-        int endmm = convertToInt(op_end.substring(0, 2));
-        int endss = convertToInt(op_end.substring(3, 5));
+        int max = convertToSec(video_len);
+        int curr = convertToSec(pos);
+        int start = convertToSec(op_start);
+        int end = convertToSec(op_end);
         
-        // 오프닝 위치이면 오프닝 끝날 때로 이동
-        if(startmm < endmm) {
-            if(mm == startmm) {
-                if(ss >= startss) {
-                    mm = endmm;
-                    ss = endss;
+        // 현재 위치가 오프닝 위치인지 확인
+        if(curr >= start && curr < end) {
+            curr = end;
+        }
+        
+        for(String cmd : commands) {
+            if(cmd.equals("next")) {
+                curr += 10;
+                if(curr > max) {
+                    curr = max;
                 }
-            } else if(mm > startmm && mm < endmm) {
-                mm = endmm;
-                ss = endss;
-            } else if(mm == endmm) {
-                if(ss < endss) {
-                    ss = endss;
+                // 현재 위치가 오프닝 위치인지 확인
+                if(curr >= start && curr < end) {
+                    curr = end;
                 }
-            }
-        } else { // 로직 상 startmm == endmm
-            if(mm == endmm) {
-                if(ss >= startss && ss < endss) {
-                    ss = endss;
+            } else {
+                curr -= 10;
+                if(curr < 0) {
+                    curr = 0;
+                }
+                // 현재 위치가 오프닝 위치인지 확인
+                if(curr >= start && curr < end) {
+                    curr = end;
                 }
             }
         }
-
-        for(int i=0; i<commands.length; i++) {
-            System.out.println(mm + " : " + ss);
-            if(commands[i].equals("next")) {
-                ss += 10;
-                if(ss>=60) {
-                    mm++;
-                    ss -= 60;
-                }
-                // 최대 위치 넘어갔나 확인
-                if(mm>maxmm) {
-                    mm = maxmm;
-                    ss = maxss;
-                }
-                if(mm==maxmm && ss>maxss) {
-                    ss = maxss;
-                } 
-                // // 오프닝 위치이면 오프닝 끝날 때로 이동
-                if(startmm < endmm) {
-                    if(mm == startmm) {
-                        if(ss >= startss) {
-                            mm = endmm;
-                            ss = endss;
-                        }
-                    } else if(mm > startmm && mm < endmm) {
-                        mm = endmm;
-                        ss = endss;
-                    } else if(mm == endmm) {
-                        if(ss < endss) {
-                            ss = endss;
-                        }
-                    }
-                } else { // 로직 상 startmm == endmm
-                    if(mm == endmm) {
-                        if(ss >= startss && ss < endss) {
-                            ss = endss;
-                        }
-                    }
-                }
-
-            }
-            else if(commands[i].equals("prev")) {
-                ss -= 10;
-                if(ss<0) {
-                    mm--;
-                    ss += 60;
-                }
-                // 00:00보다 작은지 확인
-                if(mm<0) {
-                    mm = 0;
-                    ss = 0;
-                }
-                
-                // 오프닝 위치이면 오프닝 끝날 때로 이동
-                // 오프닝 - 10:15 ~ 11:10
-                // 현재눙 - 10:23
-                // 오프닝 - 10:15 ~ 11:17
-                // 현재눙 - 10:23
-                // 오프닝 - 10:10 ~ 11:56
-                // 현재눙 - 11:23
-                // 오프닝 - 10:00 ~ 10:56
-                // 현재눙 - 10:12
-                if(startmm < endmm) {
-                    if(mm == startmm) {
-                        if(ss >= startss) {
-                            mm = endmm;
-                            ss = endss;
-                        }
-                    } else if(mm > startmm && mm < endmm) {
-                        mm = endmm;
-                        ss = endss;
-                    } else if(mm == endmm) {
-                        if(ss < endss) {
-                            ss = endss;
-                        }
-                    }
-                } else { // 로직 상 startmm == endmm
-                    if(mm == endmm) {
-                        if(ss >= startss && ss < endss) {
-                            ss = endss;
-                        }
-                    }
-                }
-            }
+        // 현재 초를 문자열로 바꾸기
+        int m = curr/60;
+        int s = curr%60;
+        String mm = "";
+        String ss = "";
+        if(m<10) {
+            mm = "0"+m;
+        } else {
+            mm = m+"";
         }
-        
-        
-        String mmStr = convertToString(mm);
-        String ssStr = convertToString(ss);
-        System.out.println(mmStr + ":" + ssStr);
-        return mmStr+":"+ssStr;
-
+        if(s<10) {
+            ss = "0"+s;
+        } else {
+            ss = s+"";
+        }
+        return mm+":"+ss;
     }
-    static int convertToInt(String str) {
-        return 10*(str.charAt(0)-'0') + (str.charAt(1)-'0');
-    }
-    static String convertToString(int num) {
-        if(num<10) {
-            return "0"+num;
-        }
-        return num+"";
+    static int convertToSec(String time) {
+        char[] arr = time.toCharArray();
+        int m = (arr[0]-'0')*10 + (arr[1]-'0');
+        int s = (arr[3]-'0')*10 + (arr[4]-'0');
+        return m*60+s;
     }
 }
