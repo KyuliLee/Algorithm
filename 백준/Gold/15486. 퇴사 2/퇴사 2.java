e6df2f6d837b784c8b9a5d26381912e84d6f7cab
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2025-01-02 21:27:53 +0900

    [Gold V] Title: 퇴사 2, Time: 680 ms, Memory: 316256 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] T;
+    static int[] P;
+    static int[] best;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        T = new int[N+1];
+        P = new int[N+1];
+        best = new int[N+2];
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        } // 초기화 완료
+        int max = 0;
+        for(int i=1; i<=N; i++) {
+            best[i] = Math.max(best[i], best[i-1]);
+            if(i+T[i]<=N+1) {
+                best[i+T[i]] = Math.max(best[i]+P[i], best[i+T[i]]);
+            }
+        }
+//        System.out.println(best[N+1]);
+        System.out.println(Arrays.stream(best).max().getAsInt());
+    }
+}
