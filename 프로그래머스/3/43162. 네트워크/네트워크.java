16f3f3f38f955754c2b65f61fef7ef9fc96bbed2
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2025-03-19 12:05:26 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -14,10 +14,8 @@ class Solution {
         }
         for(int i=0; i<size; i++) {
             int[] conn = computers[i];
-            System.out.println(Arrays.toString(conn));
             for(int j=i+1; j<size; j++) {
                 if(conn[j]==1) {
-                    System.out.println("start : " + i + ", end : " + j);
                     list[i].add(j);
                     list[j].add(i);
                 }
@@ -26,7 +24,6 @@ class Solution {
         
         for(int i=0; i<size; i++) {
             if(!visited[i]) {
-                System.out.println("카운트되는 점 : " + i);
                 cnt++;
                 visited[i] = true;
                 dfs(i);
@@ -35,9 +32,7 @@ class Solution {
         return cnt;
     }
     static void dfs(int com) {
-        System.out.println("dfs되는 점 : " + com);
         List<Integer> network = list[com];
-        System.out.println(network);
         
         for(int i=0; i<network.size(); i++) {
             int n = network.get(i);
