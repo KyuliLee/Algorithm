e459c8ce427b7ab635e8f977941e5334a362bce0
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2025-03-19 12:03:30 +0900

    [level 3] Title: 네트워크, Time: 22.10 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,44 +1,51 @@
 import java.util.*;
 class Solution {
-    static List<Integer>[] graph;
+    static List<Integer>[] list;
+    static int size;
     static boolean[] visited;
     static int cnt = 0;
     
     public int solution(int n, int[][] computers) {
-        graph = new List[n];
-        for(int i=0; i<n; i++) {
-            graph[i] = new ArrayList<>();
+        size = computers.length;
+        visited = new boolean[size];
+        list = new ArrayList[size];
+        for(int i=0; i<size; i++) {
+            list[i] = new ArrayList<>();
         }
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(computers[i][j]==1) {
-                    graph[i].add(j);
-                    graph[j].add(i);
+        for(int i=0; i<size; i++) {
+            int[] conn = computers[i];
+            System.out.println(Arrays.toString(conn));
+            for(int j=i+1; j<size; j++) {
+                if(conn[j]==1) {
+                    System.out.println("start : " + i + ", end : " + j);
+                    list[i].add(j);
+                    list[j].add(i);
                 }
             }
-        } // 그래프 초기화 완료
+        } // 초기화 완료
         
-        visited = new boolean[n];
-
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<size; i++) {
             if(!visited[i]) {
+                System.out.println("카운트되는 점 : " + i);
                 cnt++;
                 visited[i] = true;
-                
-                for(int com : graph[i]) {
-                    dfs(com);
-                }
+                dfs(i);
             }
         }
-        
         return cnt;
     }
     static void dfs(int com) {
-        visited[com] = true;
-        for(int newCom : graph[com]) {
-            if(!visited[newCom]) {
-                dfs(newCom);
+        System.out.println("dfs되는 점 : " + com);
+        List<Integer> network = list[com];
+        System.out.println(network);
+        
+        for(int i=0; i<network.size(); i++) {
+            int n = network.get(i);
+            if(!visited[n]) {
+                visited[n] = true;
+                dfs(n);
             }
         }
+        
     }
 }
