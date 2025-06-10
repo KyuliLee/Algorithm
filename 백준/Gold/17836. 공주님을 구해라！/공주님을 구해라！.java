a705f910b57b0c98d36549e9c3e826131c7fd6a3
KyuliLee <kllee0309@gmail.com>
2025-06-10 15:02:48 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 140 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -56,78 +56,15 @@ public class Main {
          */
 
         // 1) 그냥 bfs로 목적지까지
-        Queue<Pos> q1 = new LinkedList<>();
-        q1.offer(new Pos(0, 0, 0));
-        boolean[][] visited1 = new boolean[N][M];
-        visited1[0][0] = true;
-        int time1 = -1;
-        while(!q1.isEmpty()) {
-            Pos currPos = q1.poll();
-            int currR = currPos.r;
-            int currC = currPos.c;
-            int time = currPos.time;
-
-            if(time > T) {
-                continue;
-            }
-
-            if(currR == N-1 && currC == M-1) {
-                time1 = time;
-                break;
-            }
-            for(int d=0; d<4; d++) {
-                int newR = currR + dr[d];
-                int newC = currC + dc[d];
-
-                if(!isValid(newR, newC) || visited1[newR][newC]) {
-                    continue;
-                }
-                if(arr[newR][newC] == 1) { // 벽은 못 지나감
-                    continue;
-                }
-                q1.offer(new Pos(newR, newC, time+1));
-                visited1[newR][newC] = true;
-            }
-        }
+        int time1 = bfs(N-1, M-1);
         // 2) 그람까지 bfs
-        Queue<Pos> q2 = new LinkedList<>();
-        q2.offer(new Pos(0, 0, 0));
-        boolean[][] visited2 = new boolean[N][M];
-        visited2[0][0] = true;
-        int time2 = -1;
-        while(!q2.isEmpty()) {
-            Pos currPos = q2.poll();
-            int currR = currPos.r;
-            int currC = currPos.c;
-            int time = currPos.time;
+        int time2 = bfs(gramR, gramC);
 
-            if(time > T) {
-                continue;
-            }
-
-            if(currR == gramR && currC == gramC) {
-                time2 = time;
-                break;
-            }
-            for(int d=0; d<4; d++) {
-                int newR = currR + dr[d];
-                int newC = currC + dc[d];
-
-                if(!isValid(newR, newC) || visited2[newR][newC]) {
-                    continue;
-                }
-                if(arr[newR][newC] == 1) { // 벽은 못 지나감
-                    continue;
-                }
-                q2.offer(new Pos(newR, newC, time+1));
-                visited2[newR][newC] = true;
-            }
-        }
-
-        // time > T에서 break 했으니까 bfs1, bfs2에서 이미 시간이 T를 넘었으면 time이 -1로 고정됨
-
+        // time이 -1이면 T이내에 도달하지 못한 것임
+        // 그람까지 T 이내에 도달했으면 그 시간에 그람부터 목적지까지 가는 시간을 더함
         if(time2 != -1) {
             time2 += (N-1)-gramR + (M-1)-gramC;
+            // 만약 이 때 시간이 T를 넘는다면 -1로 갱신
             if(time2 > T) {
                 time2 = -1;
             }
@@ -144,5 +81,41 @@ public class Main {
     }
     public static boolean isValid(int r, int c) {
         return r>=0 && r<N && c>=0 && c<M;
+    }
+    public static int bfs(int destR, int destC) {
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(new Pos(0, 0, 0));
+        boolean[][] visited = new boolean[N][M];
+        visited[0][0] = true;
+        int time = -1;
+        while(!q.isEmpty()) {
+            Pos currPos = q.poll();
+            int currR = currPos.r;
+            int currC = currPos.c;
+            int currTime = currPos.time;
+
+            if(currTime > T) {
+                continue;
+            }
+
+            if(currR == destR && currC == destC) {
+                time = currTime;
+                break;
+            }
+            for(int d=0; d<4; d++) {
+                int newR = currR + dr[d];
+                int newC = currC + dc[d];
+
+                if(!isValid(newR, newC) || visited[newR][newC]) {
+                    continue;
+                }
+                if(arr[newR][newC] == 1) { // 벽은 못 지나감
+                    continue;
+                }
+                q.offer(new Pos(newR, newC, currTime +1));
+                visited[newR][newC] = true;
+            }
+        }
+        return time;
     }
 }
