ebffe3c0bdf8a4f32dcfe15e6f54c4cc79a467cc
KyuliLee <kllee0309@gmail.com>
2025-06-17 11:31:05 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 116 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -12,7 +12,7 @@ public class Main {
             for(int j=1; j<=i; j++) {
                 sb.append("*");
             }
-            for(int j=n-i; j>=1; j--) {
+            for(int j=1; j<=n-i; j++) {
                 sb.append("  ");
             }
             for(int j=1; j<=i; j++) {
@@ -20,19 +20,19 @@ public class Main {
             }
             sb.append("\n");
         }
-        for(int i=1; i<=2*n; i++) {
-            sb.append("*");
+        for(int i=1; i<=n; i++) {
+            sb.append("**");
         }
         sb.append("\n");
 
         for(int i=n-1; i>=1; i--) {
-            for(int j=i; j>=1; j--) {
+            for(int j=1; j<=i; j++) {
                 sb.append("*");
             }
             for(int j=1; j<=n-i; j++) {
                 sb.append("  ");
             }
-            for(int j=i; j>=1; j--) {
+            for(int j=1; j<=i; j++) {
                 sb.append("*");
             }
             sb.append("\n");
