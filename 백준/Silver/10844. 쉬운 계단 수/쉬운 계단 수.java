4dbb05112156458caf65ebbd203b05c1599a6d58
KyuliLee <kllee0309@gmail.com>
2025-06-24 09:51:21 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int mod = 1000000000;
+
+        /* 풀이
+        N = 1, 2, 3일 때까지 해보면 규칙을 알 수 있다.
+        1. 일의 자리가 0인 경우에 다음 단계에서는 01만 가능하다
+        2. 일의 자리가 1~8인 경우에 다음 단계에서는 10, 12, 21, 23, ..., 87, 89가 가능하다.
+        3. 일의 자리가 9인 경우에 다음 단계에서는 98만 가능하다
+        2차원 배열 생성. dp[a][b]는 길이가 a이고 끝자리가 b인 계단 수의 개수를 말한다.
+         */
+
+        long[][] dp = new long[N+1][10];
+        for(int i=1; i<=9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for(int n=1; n<N; n++) {
+            for(int i=0; i<10; i++) {
+                if(i==0) {
+                    dp[n+1][1] += (dp[n][0]%mod);
+                } else if(i==9) {
+                    dp[n+1][8] += (dp[n][9]%mod);
+                } else {
+                    dp[n+1][i-1] += (dp[n][i]%mod);
+                    dp[n+1][i+1] += (dp[n][i]%mod);
+                }
+            }
+        }
+        long num = 0;
+        for(int i=0; i<10; i++) {
+            num += (dp[N][i] % mod);
+        }
+        System.out.println(num%mod);
+    }
+}
