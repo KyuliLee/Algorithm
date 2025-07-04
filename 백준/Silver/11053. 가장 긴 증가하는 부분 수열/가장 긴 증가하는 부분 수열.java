160117494dfcf50ec442571df5d8c17e83e55af8
KyuliLee <kllee0309@gmail.com>
2025-07-04 11:42:15 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,22 +8,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int[] best = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int cnt = 0;
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int[] dp = new int[N];
         for(int i=0; i<N; i++) {
-            best[i] = 1;
+            dp[i] = 1;
 
             for(int j=0; j<i; j++) {
-                if(arr[i] > arr[j] && best[i] < best[j]+1) {
-                    best[i] = best[j]+1;
+                if(arr[j] < arr[i] && dp[i] < dp[j]+1) {
+                    dp[i] = dp[j]+1;
                 }
             }
         }
-
-        System.out.println(Arrays.stream(best).max().getAsInt());
+        int max = -1;
+        for(int i=0; i<N; i++) {
+            max = dp[i] > max ? dp[i] : max;
+        }
+        System.out.println(max);
     }
 }
