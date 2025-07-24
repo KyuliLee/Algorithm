e93fd0d56c6f2cc4cdad479dc19ee6fe0e18e5ca
KyuliLee <kllee0309@gmail.com>
2025-07-24 16:46:33 +0900

    [Silver III] Title: 순열 사이클, Time: 464 ms, Memory: 52584 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static int cnt;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for(int tc=0; tc<T; tc++) {
+            cnt = 0;
+            int N = Integer.parseInt(br.readLine());
+            arr = new int[N+1];
+            visited = new boolean[N+1];
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<=N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            } // 초기화 완료
+
+            for(int i=1; i<=N; i++) {
+                if(visited[i]) {
+                    continue;
+                }
+                dfs(i);
+                cnt++;
+            }
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void dfs(int start) {
+        visited[start] = true;
+        if(visited[arr[start]]) {
+            return;
+        }
+        dfs(arr[start]);
+    }
+}
