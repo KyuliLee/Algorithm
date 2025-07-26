6fa2e225268d374f2bf6c7a0c154e7d3f60bb11d
KyuliLee <kllee0309@gmail.com>
2025-07-26 23:11:52 +0900

    [level 2] Title: 전화번호 목록, Time: 276.95 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,17 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호를 map에 넣고 substring으로 containsKey로 찾는다
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         int N = phone_book.length;
         for(int i=0; i<N; i++) {
-            map.put(phone_book[i], 0);
+            set.add(phone_book[i]);
         }
         for(int i=0; i<N; i++) {
-            String phone = phone_book[i];
-            int numberLen = phone.length();
+            String number = phone_book[i];
+            int numberLen = number.length();
             for(int j=1; j<numberLen; j++) {
-                String temp = phone.substring(0, j);
-                if(map.containsKey(temp)) {
-                    return false;
-                }
+                String temp = number.substring(0, j);
+                if(set.contains(temp)) { return false;}
             }
         }
         return true;
