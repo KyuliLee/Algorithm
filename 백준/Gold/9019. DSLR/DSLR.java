0f7004aba84afad32095e501e9b8bb82ab23424e
KyuliLee <kllee0309@gmail.com>
2025-08-05 13:01:51 +0900

    [Gold IV] Title: DSLR, Time: 2924 ms, Memory: 299208 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -5,60 +5,97 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
+class Number {
     int n;
-    String str;
-    public Node(int n, String str) {
+    long cmd;
+    public Number(int n, long cmd) {
         this.n = n;
-        this.str = str;
+        this.cmd = cmd;
     }
 }
 public class Main {
     public static void main(String[] args) throws IOException {
-        // A를 B로 변환하는 최소한의 명령어를 나열하기
-        // Node타입의 큐를 넣고 B가 될 때까지 넣었다 뺐다 하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=T; tc++) {
+        StringBuilder sb = new StringBuilder();
+        for(int tc=0; tc<TC; tc++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            Node nodeA = new Node(A, "");
-            Queue<Node> q = new LinkedList<>();
-            q.offer(nodeA);
-            // 임의의 숫자 num에 대해서 계산했는데 중간 연산의 결과로 num이 나올 경우에 또 계산하는 것을 방지
-            boolean[] visited = new boolean[10000];
-            visited[A] = true;
-            while(!q.isEmpty()) {
-                // now의 숫자가 B와 같으면 출력하고 리턴
-                Node now = q.poll();
-                if(now.n == B) {
-                    System.out.println(now.str);
-                    break;
-                }
-                // D, S, L, R을 연산한 결과값과 str에 명령어 추가한 걸로 노드 만들고 큐에 넣음
-                int d = now.n*2 > 9999 ? now.n*2 % 10000 : now.n*2;
-                if(!visited[d]) {
-                    visited[d] = true;
-                    q.offer(new Node(d, now.str+"D"));
-                }
-                int s = now.n == 0 ? 9999 : now.n-1;
-                if(!visited[s]) {
-                    visited[s] = true;
-                    q.offer(new Node(s, now.str+"S"));
-                }
-                int l = now.n/1000 + (now.n%1000)*10;
-                if(!visited[l]) {
-                    visited[l] = true;
-                    q.offer(new Node(l, now.str+"L"));
-                }
-                int r = (now.n%10)*1000 + now.n/10;
-                if(!visited[r]) {
-                    visited[r] = true;
-                    q.offer(new Node(r, now.str+"R"));
-                }
-            }
+            int src = Integer.parseInt(st.nextToken());
+            int target = Integer.parseInt(st.nextToken());
+            long resL = bfs(src, target);
+            // D, S, L, R을 각각 1, 2, 3, 4로 하는 long을 가져옴. 이걸 D, S, L, R로 고쳐야 함
+            String res = getResult(resL);
+            sb.append(res).append("\n");
         }
+        System.out.println(sb);
+    }
+    static String getResult(long n) {
+        String ans = "";
+        while(n!=0) {
+            int k = (int) (n%10);
+            if(k==1) {
+                ans = "D"+ans;
+            } else if(k==2) {
+                ans = "S"+ans;
+            } else if(k==3) {
+                ans = "L"+ans;
+            } else {
+                ans = "R"+ans;
+            }
+            n /= 10;
+        }
+        return ans;
+    }
+    static long bfs(int src, int target) {
+        Queue<Number> q = new LinkedList<>();
+        q.offer(new Number(src, 0));
+        boolean[] visited = new boolean[10000];
+        visited[src] = true;
+
+        while(!q.isEmpty()) {
+            Number curr = q.poll();
+            if(curr.n == target) {
+                return curr.cmd;
+            }
+
+            // D
+            int newN = (curr.n*2)%10000;
+            if(!visited[newN]) {
+                q.offer(new Number(newN, curr.cmd*10 + 1));
+                visited[newN] = true;
+            }
+
+            // S
+            if(curr.n == 0) {
+                newN = 9999;
+            } else {
+                newN = curr.n-1;
+            }
+            if(!visited[newN]) {
+                q.offer(new Number(newN, curr.cmd*10 + 2));
+                visited[newN] = true;
+            }
+
+            // L
+            int d1 = curr.n/1000;
+            int d2 = (curr.n%1000)/100;
+            int d3 = (curr.n%100)/10;
+            int d4 = curr.n%10;
+            newN = d2*1000 + d3*100 + d4*10 + d1;
+            if(!visited[newN]) {
+                q.offer(new Number(newN, curr.cmd*10 + 3));
+                visited[newN] = true;
+            }
+
+            // R
+            newN = d4*1000 + d1*100 + d2*10 + d3;
+            if(!visited[newN]) {
+                q.offer(new Number(newN, curr.cmd*10 + 4));
+                visited[newN] = true;
+            }
+
+        }
+        return -1;
     }
 }
