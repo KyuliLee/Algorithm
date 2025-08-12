ff5e02e0ca85dfe8d0387b40b248b36689b9a958
KyuliLee <kllee0309@gmail.com>
2025-08-12 09:25:57 +0900

    [Gold V] Title: 집합의 표현, Time: 528 ms, Memory: 50352 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,26 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
-
+    static int[] p;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[1000001];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        p = new int[1000001];
         for(int i=0; i<=1000000; i++) {
-            arr[i] = -1;
+            p[i] = -1;
         }
         StringBuilder sb = new StringBuilder();
-        for(int tc=0; tc<m; tc++) {
+        for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int cmd = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             if(cmd == 0) {
-                uni(a, b);
+                union(a, b);
             } else {
                 int ra = find(a);
                 int rb = find(b);
@@ -34,25 +32,25 @@ public class Main {
                 }
             }
         }
-        System.out.println(sb);
-
-    }
-
-    // 이미 u와 v가 같은 그룹이라 합칠 필요가 없으면 false 리턴
-    static boolean uni(int u, int v) {
-        int ru = find(u);
-        int rv = find(v);
-        if(ru == rv) {
-            return false;
-        }
-        arr[rv] = ru;
-        return true;
+        System.out.print(sb);
     }
     static int find(int v) {
-        if(arr[v] < 0) {
+        if(p[v] < 0) {
             return v;
         }
-        int parent = arr[v];
-        return arr[v] = find(parent); // 최적화 : 부모 값을 그냥 루트 값으로 반환하게 해버림
+        return p[v] = find(p[v]);
+    }
+    static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if(x==y) {
+            return false; // 두 점의 루트가 같아서 이미 같은 그룹에 있는 경우
+        }
+        if(x > y) {
+            p[y] = x;
+        } else {
+            p[x] = y;
+        }
+        return true;
     }
 }
