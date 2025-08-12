b8db3e7cc1b1098fc5859513fd730850e63ca6a2
KyuliLee <kllee0309@gmail.com>
2025-08-12 16:15:14 +0900

    [Gold II] Title: 퍼즐, Time: 804 ms, Memory: 87348 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -3,61 +3,102 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+class Pos {
+    int r;
+    int c;
+    int cnt;
+    String value;
+    public Pos(int r, int c, int cnt, String value) {
+        this.r = r;
+        this.c = c;
+        this.cnt = cnt;
+        this.value = value;
+    }
+}
 public class Main {
     static int[][] arr = new int[3][3];
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static Map<String, Integer> map = new HashMap<>();
+    static int[] dr = {-1, 1, 0, 0}, dc = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        String str = "";
+        int zeroR = -1;
+        int zeroC = -1;
         for(int i=0; i<3; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<3; j++) {
-                str = str+Integer.parseInt(st.nextToken());
-            }
-        }
-        map.put(str, 0);
-        bfs(str);
-        if(map.containsKey("123456780")) {
-            System.out.println(map.get("123456780"));
-        } else {
-            System.out.println(-1);
-        }
-    }
-
-    static void bfs(String input) {
-        Queue<String> q = new LinkedList<>();
-        q.offer(input);
-
-        while(!q.isEmpty()) {
-            String curr = q.poll();
-            int zeroIdx = curr.indexOf("0");
-            int currR = zeroIdx/3;
-            int currC = zeroIdx%3;
-
-            for(int d=0; d<4; d++) {
-                int newR = currR+dr[d];
-                int newC = currC+dc[d];
-                if(newR<0 || newR>=3 || newC<0 || newC>=3) { continue; }
-                // 2차원 인덱스를 1차원으로 변환
-                int newIdx = 3*newR + newC;
-
-                // StringBuilder의 메서드로 숫자 바꿈
-                StringBuilder sb = new StringBuilder(curr);
-                char c = curr.charAt(newIdx);
-                sb.setCharAt(newIdx, '0');
-                sb.setCharAt(zeroIdx, c);
-
-                // 맵에 아직 없다면 방문을 안 한 것. 이 때에만 map에 넣어줌
-                String newStr = new String(sb);
-                if(!map.containsKey(newStr)) {
-                    map.put(newStr, map.get(curr)+1);
-                    q.offer(newStr);
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 0) {
+                    zeroR = i;
+                    zeroC = j;
                 }
             }
-
         }
+        /*
+        빈 칸은 bfs로 이동
+        빈 칸(0)이 움직인 후 3x3 배열을 String으로 바꿔서 Map의 키로, 그 때까지 움직인 횟수를 value로 넣음
+        만약 map에 해당 key가 있다면 그대로 종료, 만약 없다면 계속 진행
+        움직인 후가 123456780 라면 그 때의 횟수 출력
+         */
+
+        String initial = changeToString();
+        Map<String, Integer> map = new HashMap<>();
+        map.put(initial, 0);
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(new Pos(zeroR, zeroC, 0, initial));
+
+        while(!q.isEmpty()) {
+            Pos pos = q.poll();
+            String origin = pos.value;
+            // 123456780으로 배치되었는지 확인
+
+            if(origin.equals("123456780")) {
+                System.out.println(pos.cnt);
+                return;
+            }
+            // 빈 칸 이동 후 어떻게 변했는지 알기 위해서 string을 int형 2차원 배열로 늘린다
+            for(int i=0; i<3; i++) {
+                for(int j=0; j<3; j++) {
+                    arr[i][j] = origin.charAt(i*3+j)-'0';
+                }
+            }
+            for(int d=0; d<4; d++) {
+                int newR = pos.r + dr[d];
+                int newC = pos.c + dc[d];
+                if(!isValid(newR, newC)) continue;
+                // newR, newC와 빈 칸 자리 바꾸기
+                arr[pos.r][pos.c] = arr[newR][newC];
+                arr[newR][newC] = 0;
+                String changed = changeToString();
+
+                // 다음 newR, newC도 자리 바꿔야 하니까 원상복귀
+                arr[newR][newC] = arr[pos.r][pos.c];
+                arr[pos.r][pos.c] = 0;
+
+                // 이미 이동했던 위치이면 넘어감
+                if (map.containsKey(changed)) {
+                    continue;
+                }
+                q.offer(new Pos(newR, newC, pos.cnt+1, changed));
+                map.put(changed, pos.cnt+1);
+
+            }
+        }
+        System.out.println(-1);
+
+    }
+    static String changeToString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                sb.append(arr[i][j]);
+            }
+        }
+        return String.valueOf(sb);
+    }
+    static boolean isValid(int r, int c) {
+        if(r<0 || r>=3 || c<0 || c>=3) {
+            return false;
+        }
+        return true;
     }
 }
