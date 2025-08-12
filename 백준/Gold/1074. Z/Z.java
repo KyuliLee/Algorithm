ce62f85a564ae912f8b532e2f2cc7842c6790d50
KyuliLee <kllee0309@gmail.com>
2025-08-12 14:56:02 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -4,35 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        int ans = 0;
 
-        for(int i=N; i>0; i--) {
-            int half = (int)Math.pow(2, i-1);
-            int block = half*half;
 
-            // r, c가 1사분면에 있는 경우
-            if(r<half && c>=half) {
+
+        int res = 0;
+        int max = (int) Math.pow(2, N);
+        while(max > 1) {
+            int half = max /2;
+            int chunk = half*half;
+            if(r>=0 && r< half && c>=0 && c< half) {
+                max /= 2;
+            } else if(r>=0 && r< half && c>= half && c< max) {
+                res += chunk;
                 c -= half;
-                ans += block;
-            } else if(r>=half && c>=half) { // r, c가 2사분면에 있는 경우
+            } else if(r>= half && r< max && c>=0 && c< half) {
+                res += chunk*2;
+                r -= half;
+            } else {
+                res += chunk*3;
                 r -= half;
                 c -= half;
-                ans += 3*block;
-            } else if(r>=half && c<half) { // r, c가 3사분면에 있는 경우
-                r -= half;
-                ans += 2*block;
-            } else { // r, c가 4사분면에 있는 경우
-                // 아무 일도 일어나지 않음
             }
         }
-        System.out.println(ans);
+
+        System.out.println(res);
     }
-
-
 }
