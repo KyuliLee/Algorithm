982a23d9d016797a1be229376a9df519d8c47d34
KyuliLee <kllee0309@gmail.com>
2025-08-21 21:31:07 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,75 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
-class Pos{
-    int r;
-    int c;
-    int cnt;
-    public Pos(int r, int c, int cnt) {
-        this.r = r;
-        this.c = c;
-        this.cnt = cnt;
-    }
-}
 public class Main {
-    static int M, N;
+    static int N, M;
     static int[][] arr;
     static boolean[][] visited;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static int cnt;
+    static int[] dr = {-1, 1, 0, 0}, dc = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][M+1];
-        visited = new boolean[N+1][M+1];
-        for(int i=1; i<=N; i++) {
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i=0; i<N; i++) {
             String str = br.readLine();
-            for(int j=1; j<=M; j++) {
-                arr[i][j] = str.charAt(j-1)-'0';
-                if(arr[i][j] == 0) {
-                    visited[i][j] = true;
-                }
+            for(int j=0; j<M; j++) {
+                arr[i][j] = str.charAt(j)-'0';
             }
-        } // 초기화 완료
-
-        Queue<Pos> q = new LinkedList<>();
-        q.offer(new Pos(1, 1, 1));
-        visited[1][1] = true;
-
-        while(!q.isEmpty()) {
-            Pos pos = q.poll();
-            int currR = pos.r;
-            int currC = pos.c;
-            int currCnt = pos.cnt;
-
-            if(currR == N && currC == M) {
-                System.out.println(currCnt);
+        }
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[] {0, 0, 1});
+        visited[0][0] = true;
+        while(!dq.isEmpty()) {
+            int[] curr = dq.poll();
+            if(curr[0] == N-1 && curr[1] == M-1) {
+                System.out.println(curr[2]);
                 return;
             }
 
             for(int d=0; d<4; d++) {
-                int newR = currR+dr[d];
-                int newC = currC+dc[d];
-                if(!isValid(newR, newC) || visited[newR][newC]) {
-                    continue;
-                }
-                q.offer(new Pos(newR, newC, currCnt+1));
+                int newR = curr[0] + dr[d];
+                int newC = curr[1] + dc[d];
+                if(newR<0 || newR>=N || newC<0 || newC>=M || visited[newR][newC]) continue;
+                if(arr[newR][newC] == 0) continue;
+
                 visited[newR][newC] = true;
+                dq.offer(new int[] {newR, newC, curr[2]+1});
             }
-
         }
-
-
-
-    }
-    static boolean isValid(int r, int c) {
-        return r>0 && r<=N && c>0 && c<=M;
     }
 }
