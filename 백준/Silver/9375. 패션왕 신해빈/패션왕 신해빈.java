0285914feedda8349801ebbe8072367881fc77e3
KyuliLee <kllee0309@gmail.com>
2025-08-21 13:42:53 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,38 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        for(int TC=1; TC<=tc; TC++) {
-            // 의상 이름은 중복이 아니므로 저장할 필요 없음.
-            // 해당 카테고리에 의상이 몇 개인지만 저장하면 됨
-            // 카테고리 2개에 의상이 1, 2개 있다면 의상 1, null, 의상 1, 2, null 로 생각해서
-            // 2*3 에서 null, null인 경우 1개만 빼주면 됨
-            HashMap<String, Integer> map = new HashMap<>();
+        StringTokenizer st;
+        int TC = Integer.parseInt(br.readLine());
+        for(int tc=0; tc<TC; tc++) {
             int n = Integer.parseInt(br.readLine());
-            StringTokenizer st;
+            Map<String, Integer> map = new HashMap<>();
             for(int i=0; i<n; i++) {
                 st = new StringTokenizer(br.readLine());
-                st.nextToken(); // 의상 이름은 저장 안 함
-                String thisCategory = st.nextToken();
-
-                // 만약 이번 카테고리가 hashmap에 있다면 value만 1 더함
-                if(map.containsKey(thisCategory)) {
-                    map.put(thisCategory, map.get(thisCategory)+1);
-                } else { // 만약 없다면 key를 넣어줌
-                    map.put(thisCategory, 1);
-                }
+                st.nextToken();
+                String category = st.nextToken();
+                map.put(category, map.getOrDefault(category, 0)+1);
             }
-            int result = 1;
+            int res = 1;
             for(int v : map.values()) {
-                result *= (v+1);
+                res *= (v+1);
             }
-            System.out.println(result-1);
-
+            res--;
+            System.out.println(res);
         }
     }
 }
