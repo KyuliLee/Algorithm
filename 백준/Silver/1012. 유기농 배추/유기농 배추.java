98f4269dfc2270a62c7e09c73a7c28e9dc1e0ef0
KyuliLee <kllee0309@gmail.com>
2025-08-29 11:49:43 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,46 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int M, N, K;
+    static int N, M, K;
     static int[][] arr;
     static boolean[][] visited;
-    static int cnt;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int[] dr = {-1, 1, 0, 0}, dc = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for(int tc=1; tc<=T; tc++) {
+        StringBuilder sb = new StringBuilder();
+        for(int t=0; t<T; t++) {
             st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken()); // 컬럼 개수
-            N = Integer.parseInt(st.nextToken()); // 행 개수
-            K = Integer.parseInt(st.nextToken()); // 배추 위치 개수
-            cnt = 0; // 배추흰지렁이 마리 수
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
             arr = new int[N][M];
-            visited = new boolean[N][M];
             for(int i=0; i<K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int X = Integer.parseInt(st.nextToken());
-                int Y = Integer.parseInt(st.nextToken());
-                arr[Y][X] = 1;
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                arr[r][c] = 1;
             }
+            visited = new boolean[N][M];
 
-            // 0인 곳으로 못 가게 방문 처리
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<M; j++) {
-                    if(arr[i][j] == 0) {
-                        visited[i][j] = true;
-                    }
-                }
-            }
-
-            // 1인 점 찾아서 마리 수 cnt
+            // 배추섬 몇 개 있는지 찾기
             int cnt = 0;
             for(int i=0; i<N; i++) {
                 for(int j=0; j<M; j++) {
@@ -50,13 +38,14 @@ public class Main {
                     }
                 }
             }
-            System.out.println(cnt);
+            sb.append(cnt).append("\n");
         }
+        System.out.println(sb);
     }
     static void bfs(int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {r, c});
         visited[r][c] = true;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {r, c});
 
         while(!q.isEmpty()) {
             int[] curr = q.poll();
@@ -65,14 +54,13 @@ public class Main {
                 int nr = curr[0]+dr[d];
                 int nc = curr[1]+dc[d];
 
-                if(nr<0 || nr>=N || nc<0 || nc>=M) {
-                    continue;
-                }
-                if(!visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    q.offer(new int[] {nr, nc});
-                }
+                if(nr<0 || nr>=N || nc<0 || nc>=M || visited[nr][nc]) continue;
+                if(arr[nr][nc] == 0) continue;
+                visited[nr][nc] = true;
+                q.offer(new int[] {nr, nc});
             }
         }
+
     }
+
 }
