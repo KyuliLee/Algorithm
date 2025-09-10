7ecfbf32c93e239acba4c93aae81b2e120cc2f3c
KyuliLee <kllee0309@gmail.com>
2025-09-10 13:37:03 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 152 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -8,40 +8,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-        } // 초기화 완료
-        int[] dp1 = new int[N]; // 가장 긴 증가하는 부분 수열 길이 구하기
-        int[] dp2 = new int[N]; // 역순으로 봤을 때 가장 긴 증가하는 부분 수열 길이 구하기
-        // dp1, dp2 더해서 제일 큰 값 구하기.
-        // 그 값에서 1 뺌. 해당 i는 두 번 들어갔으니까
+        }
 
-        for(int n = 0; n <N; n++) {
-            dp1[n] = 1;
-            for(int i=0; i<n; i++) {
-                if(arr[i] < arr[n]) {
-                    dp1[n] = Math.max(dp1[n], dp1[i]+1);
+        int[] dp1 = new int[N];
+        int[] dp2 = new int[N];
+        int[] sum = new int[N];
+        for(int i=0; i<N; i++) {
+            dp1[i] = 1;
+
+            // i를 마지막으로 하는 가장 긴 증가하는 부분수열
+            for(int j=0; j<i; j++) {
+                if(arr[j] < arr[i] && dp1[i] < dp1[j]+1) {
+                    dp1[i] = dp1[j]+1;
+                }
+            }
+            sum[i] = dp1[i];
+        }
+        for(int i=N-1; i>=0; i--) {
+            dp2[i] = 1;
+            // i부터 시작하는 가장 긴 감소하는 부분수열
+            for(int j=N-1; j>i; j--) {
+                if(arr[j] < arr[i] && dp2[i] < dp2[j]+1) {
+                    dp2[i] = dp2[j]+1;
                 }
             }
         }
-
-        for(int n=N-1; n>=0; n--) {
-            dp2[n] = 1;
-            for(int i=N-1; i>n; i--) {
-                if(arr[n] > arr[i]) {
-                    dp2[n] = Math.max(dp2[n], dp2[i]+1);
-                }
-            }
-        }
-
-        int ans = -1;
+        int ans = 0;
         for(int i=0; i<N; i++) {
             ans = Math.max(ans, dp1[i]+dp2[i]);
         }
-        System.out.println(ans-1);
-
+        System.out.println(--ans);
 
     }
 }
