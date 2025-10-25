ea8523b5780a88b7bf20a8bb7be085a5a63d3716
KyuliLee <kllee0309@gmail.com>
2025-10-25 22:24:25 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int total = 0;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int price = a*b;
+            total += price;
+        }
+        if(total == X) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
